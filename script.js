bd046823e67800f99c1f9a9c79eb969273d8d093
koushik <kawsarmollik5@gmail.com>
2020-08-17 23:58:43 +0600

    done

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const artist = document.getElementsByClassName('artistName');
 const lyrics = document.getElementById('lyrics');
 const lyricsArtist = document.getElementById('lyrics-artist');
 const lyricsTitle = document.getElementById('lyrics-title');
-
+const bonusLyrics = document.getElementById('bonusLyrics');
+const bonusLyricsOne = document.getElementById('bonusLyricsOne');
+const bonusLyricsTwo = document.getElementById('bonusLyricsTwo');
+const bonusLyricsThree = document.getElementById('bonusLyricsThree');
 
 document.getElementById('searchBtn').addEventListener('click', function(){
     const searchLyricsName = searchLyrics.value;
@@ -34,25 +37,38 @@ const getLyrics = document.getElementsByClassName("getLyricsBtn");
 
   for (let i = 0; i < getLyrics.length; i++) {
     getLyrics[i].addEventListener('click',function(){
-    
       const songTitleName = songTitle[i].innerText;
-
       const artistName = artist[i].innerText;
 
-      lyricsArtist.innerText = artistName;
-      lyricsTitle.innerText = songTitleName;
+      if (i<10) {
+        lyricsArtist.innerText = artistName;
+        lyricsTitle.innerText = songTitleName;
 
-      lyricsSearch(artistName, songTitleName);
+      lyricsSearch(artistName, songTitleName,lyrics);
+      }
+      else if(i==10){
+        lyricsSearch(songTitleName, artistName, bonusLyrics);
+      }
+      else if(i==11){
+        lyricsSearch(songTitleName, artistName, bonusLyricsOne);
+      }
+      else if(i==12){
+        lyricsSearch(songTitleName, artistName, bonusLyricsTwo);
+      }
+      else if(i==13){
+        lyricsSearch(songTitleName, artistName, bonusLyricsThree);
+      }
     })
   }
-  function lyricsSearch(artist, title) {
+  function lyricsSearch(artist, title, lyricsDetails) {
     fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
     .then(response => response.json())
     .then(data=> {
+      
         if (data.lyrics == undefined) {
-          lyrics.innerText = "No lyrics found";
+          lyricsDetails.innerText = "No lyrics found";
         } else {
-          lyrics.innerText = data.lyrics;
+          lyricsDetails.innerText = data.lyrics;
         }
     })
   }
